ed087324adb373e322a5080262062f7b4a7c0774
Rugnar Lodbrok <rugnarlodbrok@gmail.com>
2019-10-06 15:43:44 +0300

    2d projection on 1d screen. Isometric and perspective

---
+++ js/utils.js
@@ -0,0 +1,12 @@
+function radians(degrees) {
+    return degrees * Math.PI / 180.;
+}
+
+const W = 87;
+const S = 83;
+const A = 65;
+const D = 68;
+const Q = 81;
+const E = 69;
+
+//http://keycode.info/
