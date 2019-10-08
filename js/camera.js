89b0ec67efc7327c7137bb7c3990b215316a50a5
Rugnar Lodbrok <rugnarlodbrok@gmail.com>
2019-10-08 18:46:44 +0300

    use p5 instance mode

--- js/camera.js
+++ js/camera.js
@@ -11,31 +11,31 @@ class Camera {
         this.m_inv = this.m.inverted();
     }
 
-    draw() {
+    draw(sketch) {
         let w_2 = this.screen.w / 2;
-        strokeWeight(.5);
-        stroke(200, 0, 0);
+        sketch.strokeWeight(.5);
+        sketch.stroke(200, 0, 0);
         let p1 = new Vector3(-w_2, 0);
         let p2 = new Vector3(w_2, 0);
         p1.transform(this.m);
         p2.transform(this.m);
-        line(p1.x, p1.y, p2.x, p2.y);
+        sketch.line(p1.x, p1.y, p2.x, p2.y);
         p1 = new Vector3(-w_2, 0);
         p2 = new Vector3(-w_2, 5);
         p1.transform(this.m);
         p2.transform(this.m);
-        line(p1.x, p1.y, p2.x, p2.y);
+        sketch.line(p1.x, p1.y, p2.x, p2.y);
         p1 = new Vector3(w_2, 0);
         p2 = new Vector3(w_2, 5);
         p1.transform(this.m);
         p2.transform(this.m);
-        line(p1.x, p1.y, p2.x, p2.y);
+        sketch.line(p1.x, p1.y, p2.x, p2.y);
     }
 
-    draw_projection(mesh) {
-        drawingContext.setLineDash([5, 5]);
-        strokeWeight(.3);
-        stroke(0, 0, 200);
+    draw_projection(sketch, mesh) {
+        sketch.drawingContext.setLineDash([5, 5]);
+        sketch.strokeWeight(.3);
+        sketch.stroke(0, 0, 200);
         for (let v of mesh.vertices) {
             v = v.transformed(mesh.m);
             let p = v.transformed(this.m_inv); // now we have p relative to the screen, in other words p contains screen coordinates
@@ -44,23 +44,23 @@ class Camera {
             p.x *= -this.screen.w;
             p.transform(basis_swap_yz);
             p.transform(this.m); // draw projected vertices on the screen
-            line(v.x, v.y, p.x, p.y);
+            sketch.line(v.x, v.y, p.x, p.y);
         }
-        drawingContext.setLineDash([]);
+        sketch.drawingContext.setLineDash([]);
     }
 
-    draw_on_screen(mesh) {
+    draw_on_screen(sketch, mesh) {
         let s = this.screen;
-        s.draw();
-        strokeWeight(1);
-        stroke(0, 200, 0);
-        let m = mat_mul(basis_swap_yz, this.m_inv, mesh.m);
+        s.draw(sketch);
+        sketch.strokeWeight(1);
+        sketch.stroke(0, 200, 0);
+        let m = mat_mul(basis_swap_yz, this.m_inv, mesh.m); // from world to camera coords
         for (let edge of mesh.edges) {
             let v1 = mesh.vertices[edge[0]].transformed(m);
             let v2 = mesh.vertices[edge[1]].transformed(m);
             this.projection.proj_vertex(v1);
             this.projection.proj_vertex(v2);
-            s.line(v1, v2);
+            s.line(sketch, v1, v2);
         }
     }
 }
@@ -75,7 +75,7 @@ class CamScreen {
         this.m.translate(new Vector3(w / 2, h / 2));
     }
 
-    draw() {
+    draw(sketch) {
         let p1 = new Vector3(-.5, -.5);
         let p2 = new Vector3(.5, -.5);
         let p3 = new Vector3(.5, .5);
@@ -84,21 +84,21 @@ class CamScreen {
         p2.transform(this.m);
         p3.transform(this.m);
         p4.transform(this.m);
-        strokeWeight(0);
-        fill(0);
-        beginShape();
-        vertex(p1.x, p1.y);
-        vertex(p2.x, p2.y);
-        vertex(p3.x, p3.y);
-        vertex(p4.x, p4.y);
-        endShape(CLOSE);
+        sketch.strokeWeight(0);
+        sketch.fill(0);
+        sketch.beginShape();
+        sketch.vertex(p1.x, p1.y);
+        sketch.vertex(p2.x, p2.y);
+        sketch.vertex(p3.x, p3.y);
+        sketch.vertex(p4.x, p4.y);
+        sketch.endShape(sketch.CLOSE);
     }
 
-    line(p1, p2) {
+    line(sketch, p1, p2) {
         p1 = p1.copy();
         p2 = p2.copy();
         p1.transform(this.m);
         p2.transform(this.m);
-        line(p1.x, p1.y, p2.x, p2.y);
+        sketch.line(p1.x, p1.y, p2.x, p2.y);
     }
 }
--- js/sketch.js
+++ js/sketch.js
@@ -51,15 +51,15 @@ class Mesh {
         this.edges.push([i, j]);
     }
 
-    draw() {
-        stroke(0);
-        strokeWeight(2);
+    draw(sketch) {
+        sketch.stroke(0);
+        sketch.strokeWeight(2);
         for (let edge of this.edges) {
             let p1 = this.vertices[edge[0]].copy();
             let p2 = this.vertices[edge[1]].copy();
             p1.transform(this.m);
             p2.transform(this.m);
-            line(p1.x, p1.y, p2.x, p2.y);
+            sketch.line(p1.x, p1.y, p2.x, p2.y);
         }
     }
 }
@@ -67,58 +67,61 @@ class Mesh {
 let mesh;
 let camera;
 
-function setup() {
-    createCanvas(800, 600);
-    camera = new Camera(500, 560,
-        // new Isometric(),
-        new Perspective(1, 2, 4/3, 600),
-        new CamScreen(0, 0, 300, 200));
-    camera.m.rotate(basis.k, radians(180));
-    camera.update_inv();
-    mesh = Mesh.cube(500, 400, 25);
+function p5_func(sketch) {
+    sketch.setup = () => {
+        sketch.createCanvas(800, 600);
+        camera = new Camera(500, 560,
+            // new Isometric(),
+            new Perspective(1, 2, 4 / 3, 600),
+            new CamScreen(0, 0, 300, 200));
+        camera.m.rotate(basis.k, radians(180));
+        camera.update_inv();
+        mesh = Mesh.cube(500, 400, 25);
+    };
+    sketch.draw = () => {
+        if (sketch.keyIsDown(W)) {
+            let v = new Vector3(0, 1);
+            v.transform(camera.m);
+            camera.m.set_translate(v);
+            camera.update_inv();
+        }
+        if (sketch.keyIsDown(S)) {
+            let v = new Vector3(0, -1);
+            v.transform(camera.m);
+            camera.m.set_translate(v);
+            camera.update_inv();
+        }
+        if (sketch.keyIsDown(A)) {
+            let v = new Vector3(1, 0);
+            v.transform(camera.m);
+            camera.m.set_translate(v);
+            camera.update_inv();
+        }
+        if (sketch.keyIsDown(D)) {
+            let v = new Vector3(-1, 0);
+            v.transform(camera.m);
+            camera.m.set_translate(v);
+            camera.update_inv();
+        }
+        if (sketch.keyIsDown(Q)) {
+            camera.m.rotate(basis.k, radians(-1));
+            camera.update_inv();
+        }
+        if (sketch.keyIsDown(E)) {
+            camera.m.rotate(basis.k, radians(1));
+            camera.update_inv();
+        }
+        sketch.background(220);
+        sketch.fill(0);
+        sketch.stroke(0);
+        sketch.textSize(24);
+        sketch.text("controls: W A S D Q E", 35, 235);
+        mesh.m.rotate(basis.k, radians(1));
+        mesh.draw(sketch);
+        camera.draw(sketch);
+        camera.draw_projection(sketch, mesh);
+        camera.draw_on_screen(sketch, mesh);
+    };
 }
 
-function draw() {
-    if (keyIsDown(W)) {
-        let v = new Vector3(0, 1);
-        v.transform(camera.m);
-        camera.m.set_translate(v);
-        camera.update_inv();
-    }
-    if (keyIsDown(S)) {
-        let v = new Vector3(0, -1);
-        v.transform(camera.m);
-        camera.m.set_translate(v);
-        camera.update_inv();
-    }
-    if (keyIsDown(A)) {
-        let v = new Vector3(1, 0);
-        v.transform(camera.m);
-        camera.m.set_translate(v);
-        camera.update_inv();
-    }
-    if (keyIsDown(D)) {
-        let v = new Vector3(-1, 0);
-        v.transform(camera.m);
-        camera.m.set_translate(v);
-        camera.update_inv();
-    }
-    if (keyIsDown(Q)) {
-        camera.m.rotate(basis.k, radians(-1));
-        camera.update_inv();
-    }
-    if (keyIsDown(E)) {
-        camera.m.rotate(basis.k, radians(1));
-        camera.update_inv();
-    }
-    background(220);
-    fill(0);
-    stroke(0);
-    textSize(24);
-    text("controls: W A S D Q E", 35, 235);
-    mesh.m.rotate(basis.k, radians(1));
-    mesh.draw();
-    camera.draw();
-    camera.draw_projection(mesh);
-    camera.draw_on_screen(mesh);
-}
+new p5(p5_func, document.getElementById("sketch"));
